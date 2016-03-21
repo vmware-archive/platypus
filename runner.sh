1a0cfdbd078a73d452cb287c70dd9ba77367a3a2
Grant Orchard <gorchard@vmware.com>
2016-03-22 07:31:27 +1100

    APIHOST becomes API_HOST
    Standards matter.
    
    Signed-off-by: Grant Orchard <gorchard@vmware.com>

--- runner.sh
+++ runner.sh
@@ -12,9 +12,9 @@ if [ -z ${VERSION} ]; then
   export VERSION="7"
 fi
 
-if [ -z ${APIHOST} ]; then
-  echo "APIHOST not set, defaulting to localhost."
-  export APIHOST="localhost"
+if [ -z ${API_HOST} ]; then
+  echo "API_HOST not set, defaulting to localhost."
+  export API_HOST="localhost"
 fi
 
 # check the file actually exists
@@ -27,7 +27,7 @@ if [ ! -f $API_FILE ]; then
 fi
 
 sed -i -e "s|http://petstore.swagger.io/v2/swagger.json|api-$PRODUCT-$VERSION.json|g" /app/dist/index.html
-sed -i -e "s|platypus_host|$APIHOST|g" app/dist/api-$PRODUCT-$VERSION.json
+sed -i -e "s|platypus_host|$API_HOST|g" app/dist/api-$PRODUCT-$VERSION.json
 
 echo "starting your swaggerness"
 /app/node_modules/gulp/bin/gulp.js serve
